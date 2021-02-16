918ec93a6d14ce575a6a2e1addf7e3a9639ffc72
Loïc Nestler <loic.nestler@gmail.com>
2021-02-16 15:24:47 +0100

    changed a few things

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@
  * Some predefined delays (in milliseconds).
  */
 export enum Delays {
-  Short = 500,
-  Medium = 2000,
-  Long = 5000,
+   Short = 500,
+   Medium = 2000,
+   Long = 5000
 }
 
 /**
@@ -15,18 +15,17 @@ export enum Delays {
  * @returns {Promise<string>}
  */
 function delayedHello(
-  name: string,
-  delay: number = Delays.Medium,
+   name: string,
+   delay: number = Delays.Medium
 ): Promise<string> {
-  return new Promise((resolve: (value?: string) => void) =>
-    setTimeout(() => resolve(`Hello, ${name}`), delay),
-  );
+   return new Promise((resolve: (value?: string) => void) =>
+      setTimeout(() => resolve(`Hello, ${name}`), delay)
+   );
 }
 
 // Below are examples of using ESLint errors suppression
 // Here it is suppressing missing return type definitions for greeter function
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export async function greeter(name: string) {
-  return await delayedHello(name, Delays.Long);
+   return await delayedHello(name, Delays.Long);
 }
